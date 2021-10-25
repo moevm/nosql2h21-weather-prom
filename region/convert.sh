c09b7d70b368e1caedd0b982b92778dd73627f13
Iurii <iiurka>
2021-10-25 22:46:48 +0300

    Small fix

--- region/convert.sh
+++ region/convert.sh
@@ -5,5 +5,8 @@ chmod +x ./ncdump-json
 
 for file in `find ./../region -type f -name "*.nc"`
 do
-   $CONVERTER $file | $FORMATTER > "${file::-3}.json"
+    $CONVERTER $file | $FORMATTER > "${file::-3}.json"
+    if [ "$1" == "--delete-source" ]; then
+        rm -r $file
+    fi
 done
