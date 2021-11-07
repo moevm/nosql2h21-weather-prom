458eb30f3988038353576aba9a36cba3e0a8deea
Iurii <iiurka>
2021-11-07 22:45:02 +0300

    Clean up

--- converters/convert.sh
+++ converters/convert.sh
@@ -1,9 +1,7 @@
-CONVERTER="./ncdump-json -j"
+CONVERTER="ncdump-json -j"
 FORMATTER="python -m json.tool"
 
-chmod +x ./ncdump-json
-
-for file in `find ./../region -type f -name "*.nc"`
+for file in `find ../ -type f -name "*.nc"`
 do
     $CONVERTER $file | $FORMATTER > "${file::-3}.json"
     if [ "$1" == "--delete-source" ]; then
