609eda8fb204eb3c86e3d6d4f8440053a529b4b6
Gleb <42916442+hohlik67@users.noreply.github.com>
2021-12-06 15:22:03 +0300

    commit

--- src/main/scala/net/weather/prometheus/Main.scala
+++
@@ -1,9 +0,0 @@
-package net.weather.prometheus
-
-import net.weather.prometheus.application.{AppBuilder, AppConfig}
-
-/*
-  Starting point
-*/
-
-object Main extends AppBuilder(AppConfig("application.conf")) with App
