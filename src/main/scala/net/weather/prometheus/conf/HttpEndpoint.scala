eacbf139a7c01cbc17e5d4490a2c2bead951c032
Gleb <42916442+hohlik67@users.noreply.github.com>
2021-12-06 16:18:19 +0300

    commit

--- src/main/scala/net/weather/prometheus/conf/HttpEndpoint.scala
+++
@@ -1,3 +0,0 @@
-package net.weather.prometheus.conf
-
-case class HttpEndpoint(host: String, port: Int)
--- src/main/scala/net/weather/prometheus/logging/Logging.scala
+++
@@ -1,8 +0,0 @@
-package net.weather.prometheus.logging
-
-import akka.event.LoggingAdapter
-import net.weather.prometheus.actor.WeatherActor
-
-trait Logging extends WeatherActor {
-  override val log: LoggingAdapter = system.log
-}
